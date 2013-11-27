ddd13630206e4f068e46401107a26c39463a378d
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2013-11-26 22:27:09 -0700

    loadavg should work on linux too

--- plugins/load_avg.go
+++ plugins/load_avg.go
@@ -1,5 +1,10 @@
 package plugins
 
+/*
+#include <stdlib.h>
+*/
+import "C"
+
 import (
     i "github.com/darkhelmet/goggles/influxdb"
     "runtime"
@@ -23,6 +28,18 @@ func (la *LoadAvg) Run(points chan i.P) error {
     points <- i.P{"name": "LoadAvg", "one": one, "five": five, "fifteen": fifteen, "cpus": nCPUs}
 
     return nil
+}
+
+func getloadavg() (float64, float64, float64) {
+    avg := []C.double{0, 0, 0}
+
+    C.getloadavg(&avg[0], C.int(len(avg)))
+
+    one := float64(avg[0])
+    five := float64(avg[1])
+    fifteen := float64(avg[2])
+
+    return one, five, fifteen
 }
 
 func NewLoadAvg(p Params) (Plugin, error) {
